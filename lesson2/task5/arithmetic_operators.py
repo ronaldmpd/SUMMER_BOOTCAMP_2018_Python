f023ad9f03f7c47326cdd907df73c5ac3dc26ec2
Ronald Pinto <ronald.pinto.d@gmail.com>
2018-02-02 15:47:45 -0400

    RMPD Lessson2 operadores - Lesson 3 cadenas

---
+++ lesson2/task5/arithmetic_operators.py
@@ -0,0 +1,5 @@
+number = 9.0
+
+result = number / 2
+
+print("result: " + str(result))
---
+++ lesson3/task5/slicing.py
@@ -0,0 +1,5 @@
+monty_python = "Monty_Python"
+monty = monty_python[:5]      # one or both index could be dropped. monty_python[:5] is equal to monty_python[0:5]
+print(monty)
+python = monty_python[6:11] # (P-6)(n-11) Python
+print(python)
