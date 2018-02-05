0d18009d04070ffbb9dba2e6cd1496f044a5731e
Ronald Pinto <ronald.pinto.d@gmail.com>
2018-02-05 15:38:34 -0400

    RMPD Lesson4 and Lesson5

---
+++ lesson4/task6/dict_key_value.py
@@ -0,0 +1,17 @@
+"""
+Hay muchos métodos útiles en diccionarios como keys() y values()
+"""
+
+phone_book = {"John": 123, "Jane": 234, "Jerard": 345}
+print(phone_book)
+
+
+phone_book["Jill"] = 456
+print(phone_book)
+
+keys = phone_book.keys()
+print(keys)
+
+print(phone_book.keys())
+
+print(phone_book.values())
---
+++ lesson5/task4/else_elif.py
@@ -0,0 +1,36 @@
+"""
+La instrucción else complementa la instrucción if.
+La palabra clave elif es la abreviatura de "else if".
+"""
+
+x = 28
+
+if x < 0:
+    print('x < 0')
+elif x == 0:
+    print('x is zero')
+elif x == 1:
+    print('x == 1')
+else:
+    print('Ninguno fue True')
+
+#solo if else
+if x < 0:
+    print('x < 0')
+if x == 0:
+    print('x is zero')
+if x == 1:
+    print('x == 1')
+else:
+    print('Ninguno fue True')
+
+
+
+
+
+name = "John"
+
+if name == 'John':
+    print(True)
+else:
+    print(False)
