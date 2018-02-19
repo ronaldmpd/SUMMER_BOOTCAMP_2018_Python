3a3f7f1b86b1d7e36da2838b59b850626568ad43
Ronald Pinto <ronald.pinto.d@gmail.com>
2018-02-19 15:06:57 -0400

    test

---
+++ lesson12/task1/getname.py
@@ -0,0 +1,13 @@
+import socket
+
+"""
+getname.py
+"""
+
+if __name__ == '__main__':
+    hostname = 'maps.google.com'
+    direccion = socket.gethostbyname(hostname)
+    mensaje = '{} es {}' .format(hostname, direccion)
+    print(mensaje)
+
+    
